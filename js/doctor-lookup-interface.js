138e998623bfc8f3c86488422af6f5ef1679e1a6
Erik Killops <erik.killops@gmail.com>
2017-01-06 12:04:18 -0800

    debug search and display functions. adjust doc display styling

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -4,32 +4,37 @@ function fillConditionOtions(conditions) {
   for (var i = 0; i < conditions.length; i++) {
     var condition = conditions[i];
     var conditionOption = '<option value="'+condition.uid+'">'+condition.name+'</option>';
-    $("#condition").append(conditionOption);
+    $("#condition-select").append(conditionOption);
   }
 }
 
 function displayDoctors(doctors) {
   $("#doctors").empty();
-  for (i = 0; i < doctors.length; i++) {
-    var doctor = doctors[i];
-    var doctorHtml = '<div class="doc-box">'+
+  if (doctors.length !== 0) {
+    for (i = 0; i < doctors.length; i++) {
+      var doctor = doctors[i];
+      var doctorHtml = '<div class="doc-box">'+
       '<div class="row">'+
-        '<div class="col-sm-4">'+
-          '<img class="doc-pic" src="'+doctor.profile.image_url+'" alt="profile-pic"/>'+
-          '<div class="btn btn-info btn-xs doc-info" value="'+doctor.uid+'">View Profile</div>' +
-        '</div>'+
-        '<div class="col-sm-8">'+
-          '<h4>'+doctor.profile.first_name+' '+doctor.profile.middle_name[0]+'. '+doctor.profile.last_name+' <small>'+doctor.profile.title+'</small></h4>'+
-          '<p>Specialties <em>'+doctor.specialties[0].name+'</em></p>'+
-          '<br>'+
-          '<p>'+doctor.practices[0].name+'</p>'+
-          '<p>'+doctor.practices[0].street+'</p>'+
-          '<p>'+doctor.practices[0].state+', '+doctor.practices[0].zip+'</p>'+
-        '</div>'+
+      '<div class="col-sm-4">'+
+      '<img class="doc-pic" src="'+doctor.profile.image_url+'" alt="profile-pic"/>'+
+      '<span class="btn btn-info btn-xs doc-info" value="'+doctor.uid+'">View Profile</span>' +
       '</div>'+
-    '</div>';
-    $("#doctors").append(doctorHtml);
+      '<div class="col-sm-8">'+
+      '<h4>'+doctor.profile.first_name+' '+doctor.profile.last_name+' <small>'+doctor.profile.title+'</small></h4>'+
+      '<p>Specialties <em>'+doctor.specialties[0].name+'</em></p>'+
+      '<br>'+
+      '<p>'+doctor.practices[0].name+'</p>'+
+      '<p>'+doctor.practices[0].visit_address.street+'</p>'+
+      '<p>'+doctor.practices[0].visit_address.state+', '+doctor.practices[0].visit_address.zip+'</p>'+
+      '</div>'+
+      '</div>'+
+      '</div>';
+      $("#doctors").append(doctorHtml);
+    }
+  } else {
+    $("#doctors").append('<p>No doctors match your search.</p>');
   }
+  $("#results").show();
 }
 
 $(document).ready(function() {
@@ -37,15 +42,16 @@ $(document).ready(function() {
   $("#begin, #search-again").click(function() {
     $("#welcome, #results").hide();
     if ($("#condition > option").length === 0){
-      console.log("test");
       DoctorLookup.getConditions(fillConditionOtions);
     }
     $("#condition-form").show();
   });
 
-  $("form#condition").submit(function(event){
+  $("#condition").submit(function(event){
     event.preventDefault();
-    var condition = $("#condition-select > option:selected").text();
-
+    $("#condition-form").hide();
+    var condition = $("#condition-select > option:selected").text().toLowerCase();
+    var newDoctorLookup = new DoctorLookup(condition);
+    newDoctorLookup.getDoctorsByCondition(displayDoctors);
   });
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,23 +1,24 @@
 var apiKey = require("./../.env").apiKey;
 
-function DoctorLookup() {
-  this.condition = null;
+function DoctorLookup(newCondition) {
+  this.condition = newCondition;
   this.relatedDoctorsJSON = null;
 }
 
 DoctorLookup.getConditions = function(displayFunction) {
-  $.get("https://api.betterdoctor.com/2016-03-01/conditions?user_key=" + apiKey).then(function(result){
-      displayFunction(result.data);
+  $.get("https://api.betterdoctor.com/2016-03-01/conditions?user_key=" + apiKey).then(function(response){
+      displayFunction(response.data);
     }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
 
 
-DoctorLookup.prototype.getDoctorsByCondition = function(condition, displayFunction) {
-  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+condition+ "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey).then(function(result){
-    this.relatedDoctorsJSON = response.data;
-    displayFunction(resultList);
+DoctorLookup.prototype.getDoctorsByCondition = function(displayFunction) {
+  var that = this;
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+that.condition+ "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey).then(function(response){
+    that.relatedDoctorsJSON = response.data;
+    displayFunction(that.relatedDoctorsJSON);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
