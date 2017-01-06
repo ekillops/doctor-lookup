6e0d3c843db1892b6ae59fe940267bc29bd867b9
Erik Killops <erik.killops@gmail.com>
2017-01-06 13:53:37 -0800

    add geocode api. adjust search to use user selected zip/coordinates

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -16,16 +16,19 @@ function displayDoctors(doctors) {
       var doctorHtml = '<div class="doc-box">'+
       '<div class="row">'+
       '<div class="col-sm-4">'+
-      '<img class="doc-pic" src="'+doctor.profile.image_url+'" alt="profile-pic"/>'+
+      '<div class="img-wrapper">'+
+      '<img class="doc-pic img-fluid" src="'+doctor.profile.image_url+'" alt="profile-pic"/>'+
+      '</div>'+
       '<span class="btn btn-info btn-xs doc-info" value="'+doctor.uid+'">View Profile</span>' +
       '</div>'+
-      '<div class="col-sm-8">'+
+      '<div class="col-sm-4">'+
       '<h4>'+doctor.profile.first_name+' '+doctor.profile.last_name+' <small>'+doctor.profile.title+'</small></h4>'+
       '<p>Specialties <em>'+doctor.specialties[0].name+'</em></p>'+
       '<br>'+
       '<p>'+doctor.practices[0].name+'</p>'+
-      '<p>'+doctor.practices[0].visit_address.street+'</p>'+
-      '<p>'+doctor.practices[0].visit_address.state+', '+doctor.practices[0].visit_address.zip+'</p>'+
+      '<p><small>Street:</small> '+doctor.practices[0].visit_address.street+'</p>'+
+      '<p><small>State/Zip:</small> '+doctor.practices[0].visit_address.state+', '+doctor.practices[0].visit_address.zip+'</p>'+
+      '<p><small>Phone:</small> '+((doctor.practices[0].phones.length > 1)? doctor.practices[0].phones[1].number : doctor.practices[0].phones[0].number)+'</p>'+
       '</div>'+
       '</div>'+
       '</div>';
@@ -34,7 +37,6 @@ function displayDoctors(doctors) {
   } else {
     $("#doctors").append('<p>No doctors match your search.</p>');
   }
-  $("#results").show();
 }
 
 $(document).ready(function() {
@@ -51,7 +53,10 @@ $(document).ready(function() {
     event.preventDefault();
     $("#condition-form").hide();
     var condition = $("#condition-select > option:selected").text().toLowerCase();
+    var zip = $("condition-zip").val();
     var newDoctorLookup = new DoctorLookup(condition);
     newDoctorLookup.getDoctorsByCondition(displayDoctors);
+    $("#search-condition").text(newDoctorLookup.condition);
+    $("#results").show();
   });
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,7 +1,11 @@
 var apiKey = require("./../.env").apiKey;
+var geoKey = require("./../.env").geoKey;
 
-function DoctorLookup(newCondition) {
+function DoctorLookup(newCondition, newZip) {
   this.condition = newCondition;
+  this.zip = newZip;
+  this.lat = null;
+  this.long = null;
   this.relatedDoctorsJSON = null;
 }
 
@@ -16,9 +20,18 @@ DoctorLookup.getConditions = function(displayFunction) {
 
 DoctorLookup.prototype.getDoctorsByCondition = function(displayFunction) {
   var that = this;
-  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+that.condition+ "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey).then(function(response){
-    that.relatedDoctorsJSON = response.data;
-    displayFunction(that.relatedDoctorsJSON);
+  //Get latitude and longitude from maps api
+  $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+that.zip+"&key="+geoKey).then(function(zipResponse){
+    that.lat = zipResponse.results[0].geometry.location.lat;
+    that.long = zipResponse.results[0].geometry.location.lat;
+  }).then(function(){
+    //Get doctors from doctor api
+    $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+that.condition+ "&location="+that.lat+"%2C"+that.long+"%2C%20&user_location="+that.lat+"%2C"+that.long+"&skip=0&limit=20&user_key=" + apiKey).then(function(doctorResponse){
+      that.relatedDoctorsJSON = doctorResponse.data;
+      displayFunction(that.relatedDoctorsJSON);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
